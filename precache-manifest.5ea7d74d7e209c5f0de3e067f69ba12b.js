16a1d12280f807657d231944971f4b7015826ee2
Golu <anurag.bhardwaj.cpp@gmail.com>
2020-10-05 22:22:45 +0530

    Updates

---
+++ precache-manifest.5ea7d74d7e209c5f0de3e067f69ba12b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3396700b6b267c50f7a52094167f6db0",
+    "url": "/chatapllication/index.html"
+  },
+  {
+    "revision": "80e3c4604bfc0df46ec6",
+    "url": "/chatapllication/static/css/main.b0e7a0b7.chunk.css"
+  },
+  {
+    "revision": "94e0b2ba07163d31d9ca",
+    "url": "/chatapllication/static/js/2.efefe4da.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/chatapllication/static/js/2.efefe4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e3c4604bfc0df46ec6",
+    "url": "/chatapllication/static/js/main.4293b0c4.chunk.js"
+  },
+  {
+    "revision": "a4c8f280160d1dec34f3",
+    "url": "/chatapllication/static/js/runtime-main.2f9b80d1.js"
+  }
+]);
